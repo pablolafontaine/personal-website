376a251df20e1c6d8f62d648b97a3914fa7209f3
Pablo Lafontaine <plafontaine@ryerson.ca>
2022-05-02 16:48:41 -0400

    added 404 page

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,10 @@ const NotFound = () => {
 	return (
 		<Container>
 			<Heading as="h1"> Not Found</Heading>
-			<p>The page you're looking for couldn't be found.</p>
+			<p>
+				The page you&#39;re looking for couldn&#39;t be
+				found.
+			</p>
 			<Divider mb={{ base: 2 }} mt={{ base: 1 }} />
 			<p>
 				Would you like to return to the{" "}
